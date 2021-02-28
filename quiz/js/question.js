5d4216245d58042431a3ae483e83168f92a46c6c
pp3times <73677600+pp3times@users.noreply.github.com>
2021-02-28 16:31:01 +0700

    Add files via upload

---
+++ quiz/js/question.js
@@ -0,0 +1,55 @@
+//Array of Obj
+const quiz = [
+    {
+        q:'Which month comes right before june?',
+        options:['may','september','july','august'],
+        answer:0
+    },
+    {
+        q:'What month is Poom birthday?',
+        options:['July','march','august','november'],
+        answer:3
+    },
+    {
+        q:'4 + 2 = 7 ?',
+        options:['true','false'],
+        answer:1
+    },
+    {
+        q:'4 + 4 = 8 ?',
+        options:['true','false'],
+        answer:0
+    },
+    {
+        q:'1 + 5 = 7 ?',
+        options:['true','false'],
+        answer:1
+    },
+    {
+        q:'test',
+        options:['true','false'],
+        answer:1
+    },
+    {
+        q:'How many squares are there in the following figure?',
+        options:['35','30','40','50'],
+        answer:2,
+        img:'img/square.jpg'
+    },
+    {
+        q:'Count the triangles in picture below.?',
+        options:['7','9','12','13'],
+        answer:3,
+        img:'img/triangle.jpg'
+    },
+    {
+        q:'Windows, macOS, and Linux are examples of ________.?',
+        options:['web browsers','mobile devices','filmy heroines','operating systems'],
+        answer:3
+    },
+    {
+        q:'Mark Zuckerberg is the owner of ?',
+        options:['facebook','google','linux','youtube'],
+        answer:0
+    }
+]
